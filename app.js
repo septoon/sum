5c3b95c38bf8939cede4d5f57f46e6c645d56129
septoon <tigran.darchinyan@gmail.com>
2019-05-20 01:15:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ const button = document.getElementById('button');
 
 inputFirst.value = 0.1;
 inputSecond.value = 0.2;
-
 button.addEventListener('click', () => {
   const one = parseFloat(inputFirst.value);
   const two = parseFloat(inputSecond.value);
   const sum = one + two;
 
   if (Number.isInteger(sum)) {
-    alert(sum);
+    alert(`Результат: ${sum}`);
   } else {
-    alert(sum.toFixed(1));
+    alert(`Результат: ${sum.toFixed(1)}`);
   }
 });
